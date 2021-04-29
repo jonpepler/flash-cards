cd0aa0bc358c55a13a5c1eb86ba32699d33c2e20
Jon Pepler <jonathan.pepler@gmail.com>
2021-04-29 22:47:42 +0100

    feat: show the app title

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,10 @@
 import { FC } from 'react'
-import logo from './logo.svg'
 import './App.css'
 
 export const App: FC = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer">
-        Learn React
-      </a>
+  <div className="app">
+    <header className="app-header">
+      <h1>Flash Cards</h1>
     </header>
   </div>
 )
